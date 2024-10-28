e8c2ed5fe40dac7b2c0cee6c30d1de33ff1fda07
bad1and <163727824+bad1and@users.noreply.github.com>
2024-10-28 22:19:29 +0300

    ver 0.3
    Вроде робит хз

--- main.c
+++ main.c
@@ -2,38 +2,30 @@
 #include <stdio.h>
 #include <ncurses.h>
 
-#define MAX_SIZE 11
-
 
 int position = 0;
 int n_button;
-int choice;
-
-void instruction (){
-    printw("%s", "-------------------------------------------------------\n");
-    printw("%s", "Используйте стрелки для перемещения\n");
-    printw("%s", "Вы - $\n");
-    printw("%s", "Игра начинается с крестика (Х)\n");
-    printw("%s", "X - установка крестика, O - установка нолика\n");
-    printw("%s", "DELETE - удалить значение в клетке\n");
-}
 
 void menu() {
-    printw("%s", "KrestikiNoliki by Tikhanov Oleg\n");
-    printw("%s", "ver 0.1\n");
+    printw("%s", "Spiralka by Tikhanov Oleg\n");
+    printw("%s", "ver 0.3\n");
     printw("%s", "\n");
-    printw("%s", "Перед использованием прочитайте инструкцию 2 п.\n");
+    printw("%s", "Перед использованием прочитайте инструкцию 6 п.\n");
     printw("%s", "-----------------\n");
     printw("%s", "\n");
 
-    char screen[4][35] = {
+    char screen[8][70] = {
         "1. Играть",
-        "2. Инструкция",
-        "3. Выход",
+        "2. Спираль по часовой стрелке",
+        "3. Спираль против часовой",
+        "4. Спираль изнутри по часовой",
+        "5. Спираль изнутри против часовой",
+        "6. Инструкция",
+        "7. Выход",
         "",
     };
 
-    for (int i = 0; i <= 3; ++i) {
+    for (int i = 0; i <= 7; ++i) {
         if (i == position) {
             printw("%s  %s\n", screen[i], "<---");
         } else {
@@ -42,9 +34,21 @@ void menu() {
     }
 }
 
+void instruction (){
+    printw("%s", "Spiralka by Tikhanov Oleg\n");
+    printw("%s", "ver 0.3\n");
+    printw("%s", "\n");
+    printw("%s", "Перед использованием прочитайте инструкцию 6 п.\n");
+    printw("%s", "-------------------------------------------------------\n");
+    printw("%s", "\n");
+    printw("%s", "Используйте стрелки для перемещения по меню.\n");
+    printw("%s", "Вы - $\n");
+    printw("%s", "Введите значения (ширину и высоту спирали)\n");
+    printw("%s", "Выберите пункт интересующий вас.\n");
+}
 
-// Заполнение по часовой стрелке снаружи
-void po_chas(int m, int n, int matrix[MAX_SIZE][MAX_SIZE]) {
+//по часовой стрелке снаружи
+void po_chas(int m, int n, int matrix[11][11]) {
     int top = 0, bottom = m - 1, left = 0, right = n - 1;
     int num = 1;
 
@@ -60,8 +64,8 @@ void po_chas(int m, int n, int matrix[MAX_SIZE][MAX_SIZE]) {
     }
 }
 
-// Заполнение против часовой стрелки снаружи
-void prot_chas(int m, int n, int matrix[MAX_SIZE][MAX_SIZE]) {
+//против часовой стрелке снаружи
+void prot_chas(int m, int n, int matrix[11][11]) {
     int top = 0, bottom = m - 1, left = 0, right = n - 1;
     int num = 1;
 
@@ -77,41 +81,42 @@ void prot_chas(int m, int n, int matrix[MAX_SIZE][MAX_SIZE]) {
     }
 }
 
-// Заполнение по часовой стрелке из центра
-void in_po_chas(int m, int n, int matrix[MAX_SIZE][MAX_SIZE]) {
-    int centerRow = m / 2, centerCol = n / 2;
-    int top = centerRow, bottom = centerRow, left = centerCol, right = centerCol;
-    int num = 1;
+//по часовой стрелке внутри
+void in_po_chas(int m, int n, int matrix[11][11]) {
+    int top = 0, bottom = m - 1, left = 0, right = n - 1;
+    int maxis = m*n;
 
-    while (top >= 0 && bottom < m && left >= 0 && right < n) {
-        for (int i = left; i <= right; i++) matrix[top][i] = num++;  // вправо
-        for (int i = top + 1; i <= bottom; i++) matrix[i][right] = num++;  // вниз
-        for (int i = right - 1; i >= left; i--) matrix[bottom][i] = num++;  // влево
-        for (int i = bottom - 1; i > top; i--) matrix[i][left] = num++;  // вверх
-
-        // Расширяем границы для следующего витка спирали
-        top--; bottom++; left--; right++;
+    while (top <= bottom && left <= right) {
+        for (int i = top; i <= bottom; i++) matrix[i][left] = maxis--;
+        left++;
+        for (int i = left; i <= right; i++) matrix[bottom][i] = maxis--;
+        bottom--;
+        if (left <= right) for (int i = bottom; i >= top; i--) matrix[i][right] = maxis--;
+        right--;
+        if (top <= bottom) for (int i = right; i >= left; i--) matrix[top][i] = maxis--;
+        top++;
     }
 }
 
-// Заполнение против часовой стрелки из центра
-void in_prot_chas(int m, int n, int matrix[MAX_SIZE][MAX_SIZE]) {
-    int centerRow = m / 2, centerCol = n / 2;
-    int top = centerRow, bottom = centerRow, left = centerCol, right = centerCol;
-    int num = 1;
+//против часовой стрелке снаружи
+void in_prot_chas(int m, int n, int matrix[11][11]) {
+    int maxis = m*n;
+    int top = 0, bottom = m - 1, left = 0, right = n - 1;
 
-    while (top >= 0 && bottom < m && left >= 0 && right < n) {
-        for (int i = top; i <= bottom; i++) matrix[i][left] = num++;  // вниз
-        for (int i = left + 1; i <= right; i++) matrix[bottom][i] = num++;  // вправо
-        for (int i = bottom - 1; i >= top; i--) matrix[i][right] = num++;  // вверх
-        for (int i = right - 1; i > left; i--) matrix[top][i] = num++;  // влево
-
-        // Расширяем границы для следующего витка спирали
-        top--; bottom++; left--; right++;
+    while (top <= bottom && left <= right) {
+        for (int i = left; i <= right; i++) matrix[top][i] = maxis--;
+        top++;
+        for (int i = top; i <= bottom; i++) matrix[i][right] = maxis--;
+        right--;
+        if (top <= bottom) for (int i = right; i >= left; i--) matrix[bottom][i] = maxis--;
+        bottom--;
+        if (left <= right) for (int i = bottom; i >= top; i--) matrix[i][left] = maxis--;
+        left++;
     }
 }
 
-void print_done(int m, int n, int matrix[MAX_SIZE][MAX_SIZE]) {
+//вывод
+void print_done(int m, int n, int matrix[11][11]) {
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             printw("%4d", matrix[i][j]);
@@ -121,21 +126,13 @@ void print_done(int m, int n, int matrix[MAX_SIZE][MAX_SIZE]) {
 }
 
 
-void mini_instruction() {
-    printw("Выберите тип спирали spiral:\n");
-    printw("1 - Снаружи по часовой\n");
-    printw("2 - Снаружи против часовой\n");
-    printw("3 - Изнутри по часовой\n");
-    printw("4 - Изнутри против часовой\n");
-    scanw("%d", &choice);
-    clear();
-}
-
 
 int main() {
     setlocale(LC_ALL, "");
     initscr();
-    int m, n;
+    int m = 0, n = 0;
+    int matrix[11][11] = {0};
+
 
     while (true) {
         menu();
@@ -149,11 +146,11 @@ int main() {
                 if (n_button == 65) {
                     position = position - 1;
                     if (position < 0) {
-                        position = 2;
+                        position = 6;
                     }
                 } else if (n_button == 66) {
                     position = position + 1;
-                    if (position > 2) {
+                    if (position > 6) {
                         position = 0;
                     }
                 }
@@ -161,7 +158,7 @@ int main() {
         }
 
        //MAIN_Ы
-        if ((position == 0 && n_button == 10)) {
+        if ((position == 0 && n_button == 10) || n_button == 49) {
             clear();
             while (true) {
                 printw("Введите количество столбцов (1-11): ");
@@ -185,38 +182,66 @@ int main() {
                     break;
                 }
             }
+        }
 
+        //По часовой
+        if ((position == 1 && n_button == 10) || n_button == 50) {
             clear();
-            mini_instruction();
-
-            int matrix[MAX_SIZE][MAX_SIZE] = {0};
-
-            switch (choice) {
-                case 1:
-                    po_chas(m, n, matrix);
-                break;
-                case 2:
-                    prot_chas(m, n, matrix);
-                break;
-                case 3:
-                    in_po_chas(m, n, matrix);
-                break;
-                case 4:
-                    in_prot_chas(m, n, matrix);
-                break;
-                default:
-                    printw("Invalid choice.\n");
+            if (m == 0 || n == 0 || (m == 0 && n == 0)) {
+                printw("\nВы ничего не ввели...");
                 getch();
-                endwin();
-                return 1;
+            }else {
+                po_chas(m, n, matrix);
+                print_done(m, n, matrix);
+                printw("\nДля выхода нажмите любую кнопку...");
+                getch();
             }
+        }
 
-            print_done(m, n, matrix);
-            getch();
+        //Против часовой
+        if ((position == 2 && n_button == 10) || n_button == 51) {
+            clear();
+            if (m == 0 || n == 0 || (m == 0 && n == 0)) {
+                printw("\nВы ничего не ввели...");
+                getch();
+            }else {
+                prot_chas(m, n, matrix);
+                print_done(m, n, matrix);
+                printw("\nДля выхода нажмите любую кнопку...");
+                getch();
+            }
+        }
+
+        //По часовой внутри
+        if ((position == 3 && n_button == 10) || n_button == 52) {
+            clear();
+            if (m == 0 || n == 0 || (m == 0 && n == 0)) {
+                printw("\nВы ничего не ввели...");
+                getch();
+            }else {
+                in_po_chas(m, n, matrix);
+                print_done(m, n, matrix);
+                printw("\nДля выхода нажмите любую кнопку...");
+                getch();
+            }
+        }
+
+        //Против часовой внутри
+        if ((position == 4 && n_button == 10) || n_button == 53) {
+            clear();
+            if (m == 0 || n == 0 || (m == 0 && n == 0)) {
+                printw("\nВы ничего не ввели...");
+                getch();
+            }else {
+                in_prot_chas(m, n, matrix);
+                print_done(m, n, matrix);
+                printw("\nДля выхода нажмите любую кнопку...");
+                getch();
+            }
         }
 
         //Instruction
-        if ((position == 1 && n_button == 10)) {
+        if ((position == 5 && n_button == 10) || n_button == 54) {
             clear();
             instruction();
             n_button = getch();
@@ -227,7 +252,7 @@ int main() {
         }
 
 
-        if ((position == 2 && n_button == 10) || n_button == 27) {
+        if ((position == 6 && n_button == 10) || n_button == 55) {
             break;
         }
 
